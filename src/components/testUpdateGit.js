fa144df71524c31dc8c494ee84969eaec6e048c0
yucheng9314 <80253822+yucheng9314@users.noreply.github.com>
2021-11-28 23:02:57 +0800

    修改测试提交

--- src/components/testUpdateGit.js
+++ src/components/testUpdateGit.js
@@ -1,24 +1,12 @@
 import React from "react";
 
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
 function testUpdateGit(props) {
-    return (
-        <div>
-            test!
-         test! test!
-         test!
-         test!
-         test!
-         test!
-        </div>
-    )
+  return <div>test! test! test! test! test! test! test!</div>;
 }
 
-testUpdateGit.propTypes = {
+testUpdateGit.propTypes = {};
 
-}
-
-export default testUpdateGit
-
+export default testUpdateGit;
